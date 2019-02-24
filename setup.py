4b5adae045a6ff73fc08438f6415569aa6fc0f77
PRATIKSHIRBHATE <pratikshirbhate@gmail.com>
2019-02-24 16:26:46 +0530

    Added an option to mention package dependecies

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name='src',
-    packages=find_packages(),
+    packages=find_packages(), #same as name- ['src']
     version='0.1.0',
     description='this is the project to test how cookiecutter works',
+    install_requires = [], #external packages as dependencies
     author='pratik',
     license='',
 )
